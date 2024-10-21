0f6c6d197047d1e4db01b7280cf42d159272d150
Dileep Rajoriya <139870968+Er-Dileep-rajoriya@users.noreply.github.com>
2024-10-21 19:53:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-function getFormvalue() {
-    //Write your code here
+function getFormvalue(event) {
+        // Prevent default form submission
+        event.preventDefault();
 
+        // Get the form element
+        let form = document.getElementById("form1");
+
+        // Create a new FormData object
+        let formData = new FormData(form);
+
+        // Get the values from the form
+        let fname = formData.get("fname");
+        let lname = formData.get("lname");
+
+        // Display the values using alert
+        alert(fname + " " + lname);
+
+        // Return false to stop form submission
+        return false;
 }
